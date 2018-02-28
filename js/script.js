8e25797365f8e71b2e8098e2d96095227268b068
mnap <learn@10g.pl>
2018-02-28 20:53:46 +0100

    Fix data validation, add test value

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
 
 function getTriangleArea (a, h) {
-    if ((a <= 0) && (h <= 0)) {
-        return 'Not valid value.';
+    if ((a <= 0) || (h <= 0)) {
+        return 'Not a valid value.';
     } else {
         return a * h / 2;
     }
@@ -12,4 +12,4 @@ console.log(getTriangleArea(10, 6));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(8, 10);
-var triangle3Area = getTriangleArea(6, 4);
+var triangle3Area = getTriangleArea(-6, 4);
