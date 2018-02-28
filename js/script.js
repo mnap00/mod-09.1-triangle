405e3c7c8c161d9f17a998eb1088c888af83e164
mnap <learn@10g.pl>
2018-02-28 20:48:10 +0100

    Add scripts and html/css files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+
+function getTriangleArea (a, h) {
+    if ((a <= 0) && (h <= 0)) {
+        return 'Not valid value.';
+    } else {
+        return a * h / 2;
+    }
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(8, 10);
+var triangle3Area = getTriangleArea(6, 4);
